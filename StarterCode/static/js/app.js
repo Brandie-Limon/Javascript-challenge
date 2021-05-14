89390976cf560d9c88f5141435e566d90b8501d4
Brandie <limon007@cougars.csusm.edu>
2021-05-13 18:10:25 -0700

    Java homework

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -30,3 +30,31 @@ tableData.forEach((UFO) => {
   });
 });
 
+// Select the button
+var button = d3.select("#filter-btn")
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#patient-form-input");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+    console.log(people);
+  
+    var filteredData = people.filter(person => person.bloodType === inputValue);
+  
+    console.log(filteredData);}
